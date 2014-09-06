aba95df84f9898bfa91dc9adb66bd2e576942a8b
Ryan Zhang <ryanzhanghouse@gmail.com>
2014-09-06 12:51:14 +0800

    User model(including secure passwords)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.4'
 gem 'bootstrap-sass', '2.3.2.0'
+gem 'bcrypt', '3.1.7'
 
 group :development, :test do
 	gem 'sqlite3', '1.3.8'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,33 +2,55 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-  #let(:base_title) { "Ruby on Rails Tutorial" }
+  subject { page }
+
+  shared_examples_for "all static pages" do
+  	it { should have_content(heading) }
+  	it { should have_title(page_title) }
+  end
 
   describe "Home page" do
-    it "should have the right title" do
-      visit root_path
-      expect(page).to have_title("Home")
-    end
+    before { visit root_path }
+
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-  	it "should have the right title" do
-  	  visit help_path
-  	  expect(page).to have_title("Help")
-  	end
+  	before { visit help_path }
+
+  	let(:heading) { 'Help' }
+  	let(:page_title) { 'Help' }
   end
 
   describe "About page" do
-  	it "should have the right title" do
-  	  visit about_path
-  	  expect(page).to have_title("About")
-  	end
+  	before { visit about_path }
+  	
+  	it { should have_selector('h1', text: 'About') }
+  	it { should have_title('About') }
   end
 
   describe "Contact page" do
-  	it "should have the right title" do
-  	  visit contact_path
-  	  expect(page).to have_title("Contact")
-  	end
+  	before { visit contact_path }
+
+  	it { should have_selector('h1', text: 'Contact') }
+  	it { should have_title('Contact') }
+  end
+
+  it "should have the right links on the layout" do
+  	visit root_path
+  	click_link "About"
+  	expect(page).to have_title('About')
+  	click_link "Help"
+  	expect(page).to have_title('Help')
+  	click_link "Contact"
+  	expect(page).to have_title('Contact')
+  	click_link "Home"
+  	click_link "Sign up now!"
+  	expect(page).to have_title('Sign')
+  	click_link "sample app"
+  	expect(page).to have_title('Home')
   end
 end
